2b84ae1e58fe4e8f437033e8ae15c96bfe556fef
sumopoops <11431120+sumopoops@users.noreply.github.com>
2022-07-15 02:27:08 -0700

    Draw correctly on resize

--- main.c
+++ main.c
@@ -4,19 +4,24 @@ int main() {
 
     // Vars
     Color colors[5];
+    int colorCount = 5;
+    int windowWidth = 500;
+    int windowHeight = 300;
 
     // Temp
     int testVar = 1;
     for (int i=0; i<5; i++) colors[i] = (Color){i*50, i*50, i*50, 255};
 
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(500, 300, "Color Finder");
+    InitWindow(windowWidth, windowHeight, "Color Finder");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
 
         // Update
         SetWindowTitle(TextFormat("%i", testVar));
+        windowWidth = GetScreenWidth();
+        windowHeight = GetScreenHeight();
         testVar++;
 
         BeginDrawing();
@@ -25,7 +30,7 @@ int main() {
 
             // Draw colors
             for (int i=0; i<5; i++)
-            DrawRectangle(i*100, 0, 100, 300, colors[i]);
+            DrawRectangle(i*(windowWidth/5), 0, (windowWidth/5), (windowHeight), colors[i]);
 
         EndDrawing();
     }
